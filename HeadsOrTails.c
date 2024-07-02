0cf08ba9b69cda53bd47b49ae6079ffddacdb0e0
Renoruiga <137611555+Renoruiga@users.noreply.github.com>
2024-07-02 12:50:19 +0900

    HoT: initial revition

---
+++ HeadsOrTails.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int tossing(int, int);
+
+int main() {
+  int i, r, h = 0, t = 0;
+
+  srand((unsigned int)time(NULL));
+
+  printf("Tossing a coin...\n");
+  
+  for (i = 0; i < 3; i++) {
+    printf("Round %d: ", i + 1);
+    
+    r = tossing(0, 1);
+
+    if(r == 0) {
+      printf("Heads\n");
+      h++;
+    } else {
+      printf("Tails\n");
+      t++;
+    }
+  }
+
+  printf("Heads: %d, Tails: %d\n", h, t);
+  
+  return 0;
+}
+
+int tossing(int H, int T) {
+  return H + (int)(rand() * (T - H + 1.0) / (1.0 + RAND_MAX));
+}
